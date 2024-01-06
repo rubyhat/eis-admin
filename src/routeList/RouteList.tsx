635218c4c01944c7d948d0a7911328a0175803cf
Vladislav <dreamervt8@gmail.com>
2024-01-06 23:28:29 +0600

    init help page

--- src/routeList/RouteList.tsx
+++ src/routeList/RouteList.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 const ProtectedRoute = React.lazy(() => import("./ProtectedRoute"));
 const Home = React.lazy(() => import("../pages/Home"));
+const Help = React.lazy(() => import("../pages/Help"));
 const Login = React.lazy(() => import("../pages/Login"));
 const Catalog = React.lazy(() => import("../pages/Catalog"));
 const CreateEstate = React.lazy(() => import("../pages/CreateEstate"));
@@ -17,6 +18,7 @@ export const RouteList = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/help" element={<Help />} />
       <Route path="/catalog" element={<Catalog />} />
       <Route path="/catalog/:id" element={<EstateDetails />} />
       <Route path="/catalog/create" element={<CreateEstate />} />
