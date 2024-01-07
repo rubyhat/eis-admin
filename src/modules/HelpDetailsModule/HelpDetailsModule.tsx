9644cd37a67eb890ccd576bf0d72dfcc13475585
Vladislav <dreamervt8@gmail.com>
2024-01-07 21:48:57 +0600

    init faq accordion

--- src/modules/HelpDetailsModule/HelpDetailsModule.tsx
+++ src/modules/HelpDetailsModule/HelpDetailsModule.tsx
@@ -1,11 +1,19 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import { FaqAccordion } from "./components/FaqAccordion";
 
 export const HelpDetailsModule = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={12}></Grid>
+        <Grid item xs={12}>
+          <Typography component="h1" variant="titleFirstRegular">
+            Объекты недвижимости
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <FaqAccordion />
+        </Grid>
       </Grid>
     </Container>
   );
---
+++ src/modules/HelpDetailsModule/components/FaqAccordion/FaqAccordion.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FaqAccordion = () => {
+  return <div>FaqAccordion</div>;
+};
