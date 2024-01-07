0457180d925580917b9e8c0c6086865e77f676fa
Vladislav <dreamervt8@gmail.com>
2024-01-07 22:28:48 +0600

    add faq accordion

--- src/modules/HelpDetailsModule/store/HelpDetailsStore.ts
+++ src/modules/HelpDetailsModule/store/HelpDetailsStore.ts
@@ -1,5 +1,22 @@
 import { create } from "zustand";
 
-export interface HelpDetailsStore {}
+import { initialState } from "./initialState";
+export interface FaqItem {
+  title: string;
+  text: string;
+  subtitle?: string;
+}
 
-export const useHelpDetailsStore = create<HelpDetailsStore>(() => ({}));
+export interface EstateFaq {
+  title: string;
+  subtitle?: string;
+  faqItems: FaqItem[];
+}
+
+export interface HelpDetailsStore {
+  estate: EstateFaq;
+}
+
+export const useHelpDetailsStore = create<HelpDetailsStore>(() => ({
+  ...initialState,
+}));
