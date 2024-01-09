cdb0991bceb48f1f578fcf8bf6f3547545db98da
Vladislav <dreamervt8@gmail.com>
2024-01-09 21:26:44 +0600

    add quill editor

--- src/modules/EstateFormModule/components/RichTextEditorField/RichTextEditorField.tsx
+++ src/modules/EstateFormModule/components/RichTextEditorField/RichTextEditorField.tsx
@@ -1,5 +1,28 @@
+import { Box } from "@mui/material";
 import React from "react";
+import ReactQuill from "react-quill";
+
+import "react-quill/dist/quill.snow.css";
 
 export const RichTextEditorField = () => {
-  return <div>RichTextEditorField</div>;
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Box
+      sx={{
+        marginBottom: 1.5,
+        "& .quill": {
+          "& .ql-toolbar": {
+            borderRadius: "8px 8px 0 0",
+          },
+          "& .ql-container, .ql-editor": {
+            minHeight: 200,
+            borderRadius: "0 0 8px 8px",
+          },
+        },
+      }}
+    >
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
+    </Box>
+  );
 };
