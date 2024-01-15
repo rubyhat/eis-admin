a6a405978bb7d33eb3076c48dcbe12bb33c80f21
Vladislav <dreamervt8@gmail.com>
2024-01-15 13:31:01 +0600

    add customer interface

--- src/modules/CustomerModule/store/useCustomerStore.ts
+++ src/modules/CustomerModule/store/useCustomerStore.ts
@@ -1,4 +1,23 @@
 import { create } from "zustand";
+import { HouseConditionType } from "../../CatalogModule/store";
+
+export interface CustomerFormFields {
+  firstname: string; // Имя
+  lastname: string; // Фамилия
+  phone: string; // Телефон
+  description: string; // Текстареа, заметки/описание запроса
+  // Хар-ки недвижимости
+  roomCount: string; // Количество комнат
+  city: string; // Город
+  priceStart: number; // Цена от
+  priceEnd: number; // Цена до
+  houseCondition: HouseConditionType; // Состояние недвижимости
+}
+
+// Как вариант отображать только количество, а если нужно их посмотреть, то могу использовать хар-ки недвижимости, чтобы открыть /catalog и передать значения в фильтр
+export interface Customer extends CustomerFormFields {
+  estateMatchCount: number; // Количество подходящих объектов под запрос этого клиента
+}
 
 export interface CustomerStore {}
 
