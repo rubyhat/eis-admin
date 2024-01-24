5726d61c0f4a663c8f190978d2a038b7673ebfe7
Vladislav <dreamervt8@gmail.com>
2024-01-24 21:28:50 +0600

    add button create new user

--- src/modules/UsersModule/UsersModule.tsx
+++ src/modules/UsersModule/UsersModule.tsx
@@ -1,16 +1,30 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import { UserList } from "./components/UserList";
+import { CustomButton } from "../../components/CustomButton";
+import { useNavigate } from "react-router";
 
 export const UsersModule = () => {
+  const navigate = useNavigate();
+
+  const handleClickNewUserButton = () => navigate("/users/create");
   return (
     <Container>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography component="h1" variant="titleFirstRegular">
-            Пользователи EIS
-          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography component="h1" variant="titleFirstRegular">
+              Сотрудники
+            </Typography>
+            <CustomButton
+              size="small"
+              sx={{ marginLeft: 2 }}
+              onClick={handleClickNewUserButton}
+            >
+              + Новый
+            </CustomButton>
+          </Box>
         </Grid>
         <UserList />
       </Grid>
