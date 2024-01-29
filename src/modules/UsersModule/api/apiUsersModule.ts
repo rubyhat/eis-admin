47effb0fbd12ed974e62a8004431638951ae5c9f
Vladislav <dreamervt8@gmail.com>
2024-01-29 19:47:50 +0600

    add user delete request

--- src/modules/UsersModule/api/apiUsersModule.ts
+++ src/modules/UsersModule/api/apiUsersModule.ts
@@ -13,4 +13,15 @@ export const apiUsersModule = {
         throw error;
       });
   },
+  deleteUserById(_id: string) {
+    return axiosBaseWrap
+      .delete("/users/" + _id)
+      .then((response) => {
+        if (response.status >= 500) throw new Error("Ошибка сервера!");
+        return response.data;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  },
 };
--- src/modules/UsersModule/components/UserCard/UserCard.tsx
+++ src/modules/UsersModule/components/UserCard/UserCard.tsx
@@ -2,18 +2,38 @@ import React from "react";
 import {
   Box,
   Chip,
+  IconButton,
   List,
   ListItem,
   ListItemText,
   Typography,
 } from "@mui/material";
+import { MdDeleteOutline } from "react-icons/md";
 import { EstateAgentInfo } from "../../../../shared/interfaces/EstateObjectTypes";
+import { apiUsersModule } from "../../api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface UserCardProps {
   user: EstateAgentInfo;
 }
 
 export const UserCard = ({ user }: UserCardProps) => {
+  const queryClient = useQueryClient();
+
+  const deleteUserMutation = useMutation({
+    mutationFn: (userId: string) => apiUsersModule.deleteUserById(userId),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["usersItems"] });
+    },
+  });
+
+  const handleDeleteButtonClick = () => {
+    if (user._id) {
+      // todo: remove if
+      deleteUserMutation.mutate(user._id);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -24,10 +44,19 @@ export const UserCard = ({ user }: UserCardProps) => {
       }}
     >
       <List dense>
-        <ListItem>
+        <ListItem
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
           <Typography component="h6" variant="titleSecondEmphasized">
             {user.name} <Chip label={user.role} color="primary" size="small" />
           </Typography>
+          <IconButton color="error" onClick={handleDeleteButtonClick}>
+            <MdDeleteOutline />
+          </IconButton>
         </ListItem>
         <ListItem>
           <ListItemText primary={user.username} secondary="Никнейм" />
