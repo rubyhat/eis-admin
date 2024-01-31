c5e30516faaf21dd738486f0424dbc934b7f6f87
Vladislav <dreamervt8@gmail.com>
2024-01-31 21:47:24 +0600

    add edit user button

--- src/modules/UsersModule/components/UserCard/UserCard.tsx
+++ src/modules/UsersModule/components/UserCard/UserCard.tsx
@@ -12,6 +12,7 @@ import {
 import toast from "react-hot-toast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CiImageOff } from "react-icons/ci";
+import { FiEdit2 } from "react-icons/fi";
 import { MdDeleteOutline } from "react-icons/md";
 
 import { EstateAgentInfo } from "../../../../shared/interfaces/EstateObjectTypes";
@@ -19,6 +20,7 @@ import { apiUsersModule } from "../../api";
 import { UserDeleteDrawer } from "../UserDeleteDrawer";
 import { useUsersStore } from "../../store/useUsersStore";
 import { useUserStore } from "../../../UserModule/store/useUserStore";
+import { useNavigate } from "react-router-dom";
 
 interface UserCardProps {
   user: EstateAgentInfo;
@@ -28,6 +30,7 @@ export const UserCard = ({ user }: UserCardProps) => {
   const { setIsDeleteDrawerOpen } = useUsersStore((state) => state);
   const { user: currentUser } = useUserStore((state) => state);
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const deleteUserMutation = useMutation({
     mutationFn: (userId: string) => apiUsersModule.deleteUserById(userId),
@@ -51,6 +54,8 @@ export const UserCard = ({ user }: UserCardProps) => {
   };
 
   const handleClickDeleteButton = () => setIsDeleteDrawerOpen(true);
+  const handleClickEditButton = () =>
+    navigate("/users/edit", { state: { user } });
 
   return (
     <Box
@@ -73,7 +78,10 @@ export const UserCard = ({ user }: UserCardProps) => {
             {user.name} <Chip label={user.role} color="primary" size="small" />
           </Typography>
           {currentUser && currentUser.role === "Admin" && (
-            <>
+            <Box>
+              <IconButton onClick={handleClickEditButton} color="primary">
+                <FiEdit2 size={20} />
+              </IconButton>
               <IconButton color="error" onClick={handleClickDeleteButton}>
                 <MdDeleteOutline />
               </IconButton>
@@ -81,7 +89,7 @@ export const UserCard = ({ user }: UserCardProps) => {
                 onClick={handleClickDeleteButton}
                 onDelete={handleDeleteUser}
               />
-            </>
+            </Box>
           )}
         </ListItem>
         <ListItem>
